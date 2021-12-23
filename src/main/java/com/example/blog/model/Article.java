a556c3215f98f99571aac3d6961baa3cf78d1a17
lionel-kg <lionel77350@gmail.com>
2021-12-23 18:19:43 +0100

    article model

--- src/main/java/com/example/blog/model/Article.java
+++ src/main/java/com/example/blog/model/Article.java
@@ -1,5 +1,52 @@
 package com.example.blog.model;
 
-public class Article {
+import java.util.Date;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="articles")
+public class Article {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "article_id")
+	private int article_id;
+	
+	/*@OneToOne(fetch = FetchType.LAZY,
+            cascade =  CascadeType.ALL,
+            mappedBy = "author")
+	private Author author;*/
+	
+	private String content;
+	
+	private Date date_pub;
+	
+	public int getArticle_id() {
+		return article_id;
+	}
+	public void setArticle_id(int article_id) {
+		this.article_id = article_id;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public Date getDate_pub() {
+		return date_pub;
+	}
+	public void setDate_pub(Date date_pub) {
+		this.date_pub = date_pub;
+	}
+	
 }
